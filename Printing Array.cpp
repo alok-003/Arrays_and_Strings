80b87f37e3a5e7f5eebd046c9c100768478f60f6
alok-003 <alokchawat@gmail.com>
2024-08-21 20:13:34 +0530

    Add files via upload

---
+++ Printing Array.cpp
@@ -0,0 +1,26 @@
+//Name:Alok Chawat
+//PRN:23070123016
+//Aim:Implemention of Arrays and Strings to print the given Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+ int x[6] = {19,10,8,6,8};
+ for(int i=0;i<5;i++)
+ {
+    cout << x[i];
+    cout<< endl;
+ }
+
+    return 0;
+}
+
+
+/*Output:
+19
+10
+8
+6
+8*/
